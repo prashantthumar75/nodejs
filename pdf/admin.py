6d3352b518ff05415466398195a7acb43d3b6c82
prashantthumar75 <48546055+prashantthumar75@users.noreply.github.com>
2021-04-15 18:39:21 +0530

    added model for inline

--- pdf/admin.py
+++ pdf/admin.py
@@ -6,3 +6,23 @@ from .models import RegisterModel
 @admin.register(RegisterModel)
 class RegisterAdmin(admin.ModelAdmin):
     list_display = ['username', 'email']
+
+from django.contrib import admin
+from .models import Post, PostImage
+
+# admin.site.register(Post)
+
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageAdmin]
+    pass
+    # class Meta:
+    #     model = Post
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
+
